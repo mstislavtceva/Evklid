916db0853b5ed333520c992f7c2fa04284578ef7
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2022-11-06 21:11:01 +0300

    Табы + переделана секция Stages

--- 13_javascript/js/script.js
+++ 13_javascript/js/script.js
@@ -12,3 +12,20 @@ const swiper = new Swiper('.swiper', {
       clickable: true
   }
 });
+
+const tabsBtn = document.querySelectorAll('.stages-start__link');
+const tabsContent = document.querySelectorAll('.stages-text__tabs');
+
+tabsBtn.forEach(function (element) {
+  element.addEventListener ('click', function (e) {
+    const path = e.currentTarget.dataset.path;
+
+    tabsBtn.forEach(function(btn){ btn.classList.remove('stages-start__link--active');});
+    e.currentTarget.classList.add('stages-start__link--active');
+
+    tabsContent.forEach(function(element){ element.classList.remove('stages-text__tabs--active');});
+    document.querySelector(`[data-target="${path}"]`).classList.add('stages-text__tabs--active');
+
+    console.log('Hey!!!');
+  });
+});
