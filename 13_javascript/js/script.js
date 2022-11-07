74a45cdf49a142226764c247e0107f857f3566ee
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2022-11-07 21:34:28 +0300

    бургер меню + лупа ховер, актив, фокус

--- 13_javascript/js/script.js
+++ 13_javascript/js/script.js
@@ -29,3 +29,26 @@ tabsBtn.forEach(function (element) {
     console.log('Hey!!!');
   });
 });
+
+new Accordion('.accordion');
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header__nav');
+const menuLinks = document.querySelectorAll('.nav__link');
+
+burger.addEventListener('click', function() {
+  burger.classList.toggle('burger--active');
+  menu.classList.toggle('header__nav--active');
+
+  document.body.classList.toggle('stop-scroll');
+
+});
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function() {
+    burger.classList.remove('burger--active');
+    menu.classList.remove('header__nav--active');
+
+    document.body.classList.remove('stop-scroll');
+  });
+});
